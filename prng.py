87016040548b88dc5e1529aeec0805a6cb7d2943
adamheidrick <adamheidrick@gmail.com>
2022-09-27 14:15:00 +0200

    While loop for prng-service

--- prng.py
+++ prng.py
@@ -2,13 +2,15 @@
 import random
 source = 'prng-service.txt'
 
-with open(source, 'r') as f:
-    text = f.read()
-    f.close()
-
-if text == 'run':
-    randy = str(random.randint(1, 100))
-    with open(source, 'w') as f:
-        f.write(randy)
+while True:
+    with open(source, 'r') as f:
+        text = f.read()
         f.close()
+
+    if text == 'run':
+        randy = str(random.randint(1, 100))
+        with open(source, 'w') as f:
+            f.write(randy)
+            f.close()
+        print(randy)
 
