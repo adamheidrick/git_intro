3903fc29cd7c8659fd65a7356dd5e1f344e68fd5
adamheidrick <adamheidrick@gmail.com>
2022-09-27 22:12:06 +0200

    Minor formatting

--- imageService.py
+++ imageService.py
@@ -6,17 +6,22 @@ image_list = os.listdir(path='images')
 cwd = os.getcwd()
 images = os.listdir(path='images')
 
-while True:
-    time.sleep(1)
-    with open(source, 'r') as f:
-        text = f.read()
-        print(f'Reading .txt = {text}')
+def main():
+    while True:
+        time.sleep(1)
+        with open(source, 'r') as f:
+            text = f.read()
+            print(f'Reading .txt = {text}')
 
-    if text.isnumeric():
-        num = int(text) % num_images
+        if text.isnumeric():
+            num = int(text) % num_images
 
-        path = cwd + "\\images\\" + image_list[num]
-        print(f'Writing path {path} to .txt')
-        with open(source, 'w') as f:
-            f.write(path)
-    f.close()
+            path = cwd + "\\images\\" + image_list[num]
+            print(f'Writing path {path} to .txt')
+            with open(source, 'w') as f:
+                f.write(path)
+        f.close()
+
+
+if __name__ == "__main__":
+    main()
--- prng.py
+++ prng.py
@@ -2,16 +2,20 @@
 import random
 import time
 source = 'prng-service.txt'
+def main():
+    while True:
+        time.sleep(1)
+        with open(source, 'r') as f:
+            text = f.read()
+            print(f'Reading .txt = {text}')
 
-while True:
-    time.sleep(1)
-    with open(source, 'r') as f:
-        text = f.read()
-        print(f'Reading .txt = {text}')
+        if text == 'run':
+            with open(source, 'w') as f:
+                randy = str(random.randint(1, 100))
+                print(f'Writing Random {randy} Number to .txt')
+                f.write(randy)
+        f.close()
 
-    if text == 'run':
-        with open(source, 'w') as f:
-            randy = str(random.randint(1, 100))
-            print(f'Writing Random {randy} Number to .txt')
-            f.write(randy)
-    f.close()
+
+if __name__ == "__main__":
+    main()
